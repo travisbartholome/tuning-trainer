83300892e8bfffd92de3a14344b1ce189c97f598
travisbartholome <travis.bartholome@gmail.com>
2017-05-13 18:30:33 -0500

    Continue work on Express server

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // Sets location to look for static files
-//app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/styles"));
+app.use(express.static(__dirname + "/scripts"));
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-  res.render("intervals.html");
+  res.sendFile(__dirname + "/views/intervals.html");
 });
 
 app.get("/intervals", function(req, res) {
-  res.sendFile("intervals.html");
+  res.sendFile(__dirname + "/views/intervals.html");
 });
 
 app.get("/quiz", function(req, res) {
-  res.sendFile("quiz.html");
+  res.sendFile(__dirname + "/views/quiz.html");
 });
 
 app.listen(app.get('port'), function() {
